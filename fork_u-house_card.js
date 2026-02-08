67c1724c89b96d0f034fe37999e385e951254b00
Luis Narquel <lmnarqueldev@gmail.com>
2026-02-08 16:16:18 +0000

    fix image path

--- fork_u-house_card.js
+++ fork_u-house_card.js
@@ -170,7 +170,7 @@ class ForkUHouseCard extends HTMLElement {
 
      // --- NOWA LOGIKA WYBORU OBRAZKA ---
     _calculateImage() {
-        const path = this._config.image_path || "/local/fork_u-house_card/images/";
+        const path = this._config.image_path || "/local/images/fork_u-house_card/";
         
         // 1. Pora Dnia
         const sunState = this._hass.states[this._config.sun_entity || 'sun.sun']?.state || 'above_horizon';
